4bf38a08126dd6ac2c2f328e0c5ac4fc8c8765f4
Alaa <alaa.ismail@student.guc.edu.eg>
2022-11-05 02:25:21 +0200

    Added logo features and refactor

--- automation.py
+++ automation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import qrcode
+from PIL import Image
 import os
 import yaml
 
@@ -9,6 +10,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 
 QRCode_folder_path = os.path.abspath('QR Code Images')
+Logo_path = 'logo.png'
 
 
 # Adds header and create id & QRCode coulmns
@@ -28,26 +30,61 @@ def retrieve_info(df):
 
     return data
 
-# Creates a unique QR Code for each attendee and return path
-def make_QR_images(data_row):
+def QRCode_logo():
+    logo = Image.open(Logo_path)
+
+    basewidth = 100
+    # adjust image size
+    wpercent = (basewidth/float(logo.size[0]))
+    hsize = int((float(logo.size[1])*float(wpercent)))
+    logo = logo.resize((basewidth, hsize), Image.Resampling.LANCZOS)
+
+    return logo
+
+def add_QRCode_text(data_row, QRcode):
     text = yaml.dump(data_row, sort_keys= False)
-    qr_image = qrcode.make(text)
+    QRcode.add_data(text)
 
+def make_QRCode(QRcode):
+    QRcode.make()
+    QR_img = QRcode.make_image().convert('RGB')
+
+    return QR_img
+
+def add_QRCode_logo(QRCode_logo, QR_img):
+    pos = ((QR_img.size[0] - QRCode_logo.size[0]) // 2,
+       (QR_img.size[1] - QRCode_logo.size[1]) // 2)
+    QR_img.paste(QRCode_logo, pos)
+
+def save_QRCode(data_row, QR_img):
     id = data_row['id']
-    qr_image_path = os.path.join(QRCode_folder_path, str(id)) + '.png'
-    qr_image.save(qr_image_path)
+    qrimg_path = os.path.join(QRCode_folder_path, str(id)) + '.png'
+    QR_img.save(qrimg_path)
 
-    return qr_image_path
+    return qrimg_path
 
-def generate_qrcode(df):
+# Creates a unique QR Code for each attendee and return path
+def make_QR_images(data_row, QRCode_logo):
+
+    QRcode = qrcode.QRCode(
+        error_correction=qrcode.constants.ERROR_CORRECT_H)
+
+    add_QRCode_text(data_row, QRcode)
+
+    QR_img = make_QRCode(QRcode)
+
+    add_QRCode_logo(QRCode_logo, QR_img)
+
+    qrimg_path = save_QRCode(data_row, QR_img)
+
+    return qrimg_path
+
+def generate_qrcode(df, QRcode_logo):
     data = retrieve_info(df)
 
-    qr_image_paths =[]
-    for data_row in data:
-        path = make_QR_images(data_row)
-        qr_image_paths.append(path)
+    qr_paths =[make_QR_images(data_row, QRcode_logo) for data_row in data]
 
-    df['QRCode'] = qr_image_paths
+    df['QRCode'] = qr_paths
 
 
 if __name__ == '__main__':
@@ -61,7 +98,10 @@ if __name__ == '__main__':
         os.mkdir('QR Code Images')
     except:
         pass
-
-    generate_qrcode(df)
+    
+    QRcode_logo = QRCode_logo()
+    generate_qrcode(df, QRcode_logo)
 
     df.to_excel(file_name + '_modified.xlsx', index=False)
+
+    print('You have successfully generated QR Codes for every attendee.')
