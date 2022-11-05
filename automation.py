37f0888a2586158c3f4be9c3e318c63d9e52a937
Alaa <alaa.ismail@student.guc.edu.eg>
2022-11-05 22:27:25 +0200

    Added Hyperlinks feature

--- automation.py
+++ automation.py
@@ -12,96 +12,105 @@ os.chdir(dir_path)
 QRCode_folder_path = os.path.abspath('QR Code Images')
 Logo_path = 'logo.png'
 
+class QR_Generator:
 
-# Adds header and create id & QRCode coulmns
-def header_with_id(df):
-    df.columns = ['name', 'email']
+    def __init__(self, file_name):
+        self.df = pd.read_excel(file_name + '.xlsx', header=None, index_col=False)
 
-    df['id'] = range(1, len(df) + 1)
-    df['QRCode'] = None
+        self.header_with_id()
 
-def retrieve_info(df):
-    data = []
-    for index, row in df.iterrows():
-        data_row = {}
-        for col_name, value in row.items():
-            data_row[col_name] = value
-        data.append(data_row)
+        try:
+            os.mkdir('QR Code Images')
+        except:
+            pass
+        
+        QRcode_logo = self.QRCode_logo()
+        self.generate_qrcode(QRcode_logo)
 
-    return data
+        self.df.to_excel(file_name + '_modified.xlsx', index=False)
 
-def QRCode_logo():
-    logo = Image.open(Logo_path)
+    # Adds header and create id & QRCode coulmns
+    def header_with_id(self):
+        self.df.columns = ['Name', 'Email']
 
-    basewidth = 100
-    # adjust image size
-    wpercent = (basewidth/float(logo.size[0]))
-    hsize = int((float(logo.size[1])*float(wpercent)))
-    logo = logo.resize((basewidth, hsize), Image.Resampling.LANCZOS)
+        self.df['id'] = range(1, len(self.df) + 1)
+        self.df['QR_Link'] = None
+        
+        new_cols = ["id", "Name", "Email", "QR_Link"]
+        self.df = self.df.reindex(columns=new_cols)
+    
+    def QRCode_logo(self):
+        logo = Image.open(Logo_path)
 
-    return logo
+        basewidth = 100
+        # adjust image size
+        wpercent = (basewidth/float(logo.size[0]))
+        hsize = int((float(logo.size[1])*float(wpercent)))
+        logo = logo.resize((basewidth, hsize), Image.Resampling.LANCZOS)
 
-def add_QRCode_text(data_row, QRcode):
-    text = yaml.dump(data_row, sort_keys= False)
-    QRcode.add_data(text)
+        return logo
 
-def make_QRCode(QRcode):
-    QRcode.make()
-    QR_img = QRcode.make_image().convert('RGB')
+    def generate_qrcode(self, QRcode_logo):
+        data = self.retrieve_info()
 
-    return QR_img
+        qr_paths =[self.make_QR_images(data_row, QRcode_logo) for data_row in data]
 
-def add_QRCode_logo(QRCode_logo, QR_img):
-    pos = ((QR_img.size[0] - QRCode_logo.size[0]) // 2,
-       (QR_img.size[1] - QRCode_logo.size[1]) // 2)
-    QR_img.paste(QRCode_logo, pos)
+        self.df['QR_Link'] = qr_paths
 
-def save_QRCode(data_row, QR_img):
-    id = data_row['id']
-    qrimg_path = os.path.join(QRCode_folder_path, str(id)) + '.png'
-    QR_img.save(qrimg_path)
+    def retrieve_info(self):
+        data = []
+        for index, row in self.df.iterrows():
+            data_row = {}
+            for col_name, value in row.items():
+                data_row[col_name] = value
+            data.append(data_row)
 
-    return qrimg_path
+        return data
 
-# Creates a unique QR Code for each attendee and return path
-def make_QR_images(data_row, QRCode_logo):
+    # Creates a unique QR Code for each attendee and return path
+    def make_QR_images(self, data_row, QRCode_logo):
 
-    QRcode = qrcode.QRCode(
-        error_correction=qrcode.constants.ERROR_CORRECT_H)
+        QRcode = qrcode.QRCode(
+            error_correction=qrcode.constants.ERROR_CORRECT_H)
 
-    add_QRCode_text(data_row, QRcode)
+        self.add_QRCode_text(data_row, QRcode)
 
-    QR_img = make_QRCode(QRcode)
+        QR_img = self.make_QRCode(QRcode)
 
-    add_QRCode_logo(QRCode_logo, QR_img)
+        self.add_QRCode_logo(QRCode_logo, QR_img)
 
-    qrimg_path = save_QRCode(data_row, QR_img)
+        qrimg_path = self.save_QRCode(data_row, QR_img)
 
-    return qrimg_path
+        return qrimg_path
 
-def generate_qrcode(df, QRcode_logo):
-    data = retrieve_info(df)
+    def add_QRCode_text(self, data_row, QRcode):
+        text = yaml.dump(data_row, sort_keys= False)
+        QRcode.add_data(text)
 
-    qr_paths =[make_QR_images(data_row, QRcode_logo) for data_row in data]
+    def make_QRCode(self, QRcode):
+        QRcode.make()
+        QR_img = QRcode.make_image().convert('RGB')
 
-    df['QRCode'] = qr_paths
+        return QR_img
 
+    def add_QRCode_logo(self, QRCode_logo, QR_img):
+        pos = ((QR_img.size[0] - QRCode_logo.size[0]) // 2,
+        (QR_img.size[1] - QRCode_logo.size[1]) // 2)
+        QR_img.paste(QRCode_logo, pos)
 
+    def save_QRCode(self, data_row, QR_img):
+        id = data_row['id']
+        qrimg_path = os.path.join(QRCode_folder_path, str(id)) + '.png'
+
+        QR_img.save(qrimg_path)
+
+        hyperlink = f'=HYPERLINK("{qrimg_path}", "{id}_QR_Link")'
+        return hyperlink
+
+    
 if __name__ == '__main__':
 
     file_name = input('Enter your excel file name without the extension & Make sure it is in the same directory as the script \n')
-    df = pd.read_excel(file_name + '.xlsx', header=None, index_col=False)
-
-    header_with_id(df)
-
-    try:
-        os.mkdir('QR Code Images')
-    except:
-        pass
-    
-    QRcode_logo = QRCode_logo()
-    generate_qrcode(df, QRcode_logo)
-
-    df.to_excel(file_name + '_modified.xlsx', index=False)
+    QR_Generator(file_name)
 
     print('You have successfully generated QR Codes for every attendee.')
